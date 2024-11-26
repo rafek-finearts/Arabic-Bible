b9a66ef47d2de911b48ccc3c0d2c68267f715f42
rafek-finearts <65669754+rafek-finearts@users.noreply.github.com>
2024-11-26 21:05:56 +0200

    Fix asset paths for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  base: '',
+  base: '/arabic-bible/', // Add this line to set the base URL
   plugins: [
     react(),
     VitePWA({
